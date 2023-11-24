853fb4b216e9f640d2694a58c8dac792e9be2c87
ael-maim <ael-maim@e2r12p11.1337.ma>
2023-11-24 08:45:51 +0100

    protec lst_back and remove comment in lst_front

--- lib_ft/done/ft_lstadd_back_bonus.c
+++ lib_ft/done/ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ael-maim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 15:14:42 by ael-maim          #+#    #+#             */
-/*   Updated: 2023/11/11 15:26:59 by ael-maim         ###   ########.fr       */
+/*   Updated: 2023/11/24 08:44:11 by ael-maim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*node;
 
-	node = *lst;
 	if (lst)
 	{
+		node = *lst;
 		if (!*lst)
 			*lst = new;
 		else
