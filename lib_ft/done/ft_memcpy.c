14e8c890f75f9f54414c9206caa7ec12b10d6b90
ael-maim <ael-maim@c4r1p5.1337.ma>
2023-11-24 06:41:29 +0100

    done

---
+++ lib_ft/done/ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-maim <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 14:27:32 by ael-maim          #+#    #+#             */
+/*   Updated: 2023/11/19 09:28:59 by ael-maim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*d;
+	unsigned char	*s;
+
+	i = 0;
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
---
+++ lib_ft/done/ft_memmove.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-maim <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 15:52:48 by ael-maim          #+#    #+#             */
+/*   Updated: 2023/11/19 09:29:08 by ael-maim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*s;
+	unsigned char	*d;
+	size_t			i;
+
+	s = (unsigned char *)src;
+	d = (unsigned char *)dest;
+	i = 0;
+	if (d > s)
+		while (n-- > 0)
+			d[n] = s[n];
+	else
+		while (i++ < n && n != 0)
+			*d++ = *s++;
+	return (dest);
+}
