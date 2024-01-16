5ad3eef2d0ef1bf591d21c4a60a9150521b625f0
Abdelali El Maimouni <rimox.midox2011@gmail.com>
2024-01-16 19:41:15 +0100

    loading

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,11 +6,41 @@
 /*   By: ael-maim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 15:59:19 by ael-maim          #+#    #+#             */
-/*   Updated: 2024/01/16 18:18:40 by ael-maim         ###   ########.fr       */
+/*   Updated: 2024/01/16 19:17:51 by ael-maim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+t_list	*ft_get_pos_b(t_list **stack_a, t_list **stack_b)
+{
+	int	pos;
+	t_list	*tmp;
+
+	tmp = *stack_b;
+	pos = 1;
+	while (tmp)
+	{
+		if (!ft_compare_node(*stack_a, tmp))
+			i++;
+		else
+		    return (tmp);
+		tmp = tmp->next;
+	}
+	return (ft_lstlast(tmp));
+}
+
+void	ft_sort_stack_b(t_list	**stack_a, t_list **stack_b)
+{
+	(void)stack_a;
+	(void)stack_b;
+	
+	if (ft_lstsize(*stack_b) >= 2)
+	{
+		
+
+	}
+}
 
 int	main(int ac, char **av)
 {
@@ -36,8 +66,11 @@ int	main(int ac, char **av)
 	printf("sa\n");
     }
     ft_split_stack(&stack_a, &stack_b);
-    ft_first_sort(&stack_a, &stack_b);
-/*
+
+    ft_reverse_rotation(&stack_a);
+    ft_reverse_rotation(&stack_b);
+  //  ft_first_sort(&stack_a, &stack_b);
+///*
     t_list	*tmp;
     printf("---------------------------------\n");
     tmp = stack_a;
@@ -53,9 +86,9 @@ int	main(int ac, char **av)
 	printf("stack_B :%d \n", *(int*)tmp->content);
 	tmp = tmp->next;
     }
-*/
+//*/
 
-
+	
    // ft_rotation(&stack_a);
     tail = stack_a;
 
@@ -73,10 +106,11 @@ int	main(int ac, char **av)
 
 
 
+    ft_sort_stack_b(&stack_a, &stack_b);
 
     printf("ft_check_sort : %d\n",ft_check_sort(&stack_a, &stack_b, tail));
      ///*
-//   t_list	*tmp;
+   //t_list	*tmp;
     printf("---------------------------------\n");
     tmp = stack_a;
     while (tmp)
